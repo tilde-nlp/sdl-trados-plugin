cc61b0d82baed85b981b5c4b0f21c2f56c4f63d4
Rihards Krišlauks <rihards.krislauks@Tilde.lv>
2015-06-17 13:40:35 +0000

    Place logo on bottom.
    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/LetsMT!/SDLTradosPlugin;C27156

--- RetryWarningForm.Designer.cs
+++ RetryWarningForm.Designer.cs
@@ -33,24 +33,24 @@
             this.WrningTextLable = new System.Windows.Forms.Label();
             this.YesButton = new System.Windows.Forms.Button();
             this.NoButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.autoRetry = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // WrningTextLable
             // 
-            this.WrningTextLable.AutoSize = true;
-            this.WrningTextLable.Location = new System.Drawing.Point(78, 9);
+            this.WrningTextLable.Location = new System.Drawing.Point(12, 9);
             this.WrningTextLable.Name = "WrningTextLable";
-            this.WrningTextLable.Size = new System.Drawing.Size(236, 52);
+            this.WrningTextLable.Size = new System.Drawing.Size(355, 46);
             this.WrningTextLable.TabIndex = 0;
-            this.WrningTextLable.Text = "Your automated translation system is starting up. \r\nTranslation will start automa" +
-    "tically as soon as \r\nthe system is ready. \r\nWould you like to try again?";
+            this.WrningTextLable.Text = "Your automated translation system is starting up. Translation will start automati" +
+    "cally as soon as the system is ready. Would you like to try again?";
             // 
             // YesButton
             // 
-            this.YesButton.Location = new System.Drawing.Point(110, 75);
+            this.YesButton.Location = new System.Drawing.Point(172, 58);
             this.YesButton.Name = "YesButton";
             this.YesButton.Size = new System.Drawing.Size(75, 23);
             this.YesButton.TabIndex = 1;
@@ -61,7 +61,7 @@
             // NoButton
             // 
             this.NoButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.NoButton.Location = new System.Drawing.Point(191, 75);
+            this.NoButton.Location = new System.Drawing.Point(253, 58);
             this.NoButton.Name = "NoButton";
             this.NoButton.Size = new System.Drawing.Size(114, 23);
             this.NoButton.TabIndex = 2;
@@ -69,20 +69,30 @@
             this.NoButton.UseVisualStyleBackColor = true;
             this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::LetsMT.MTProvider.PluginResources.LetsMT_logo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 19);
-            this.pictureBox1.TabIndex = 13;
-            this.pictureBox1.TabStop = false;
-            // 
             // autoRetry
             // 
             this.autoRetry.Enabled = true;
             this.autoRetry.Interval = 15000;
             this.autoRetry.Tick += new System.EventHandler(this.autoRetry_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(13, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 12);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Powered by";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LetsMT.MTProvider.PluginResources.LetsMT_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(79, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(57, 19);
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
             // 
             // RetryWarningForm
             // 
@@ -90,8 +100,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.NoButton;
-            this.ClientSize = new System.Drawing.Size(317, 110);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(379, 93);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.NoButton);
             this.Controls.Add(this.YesButton);
             this.Controls.Add(this.WrningTextLable);
@@ -102,7 +113,7 @@
             this.Name = "RetryWarningForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "System starting up";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,7 +124,8 @@
         private System.Windows.Forms.Label WrningTextLable;
         private System.Windows.Forms.Button YesButton;
         private System.Windows.Forms.Button NoButton;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer autoRetry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
