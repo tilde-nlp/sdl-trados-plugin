bd767b1684a1330b50893a79372893f7892cefa8
Rihards Krišlauks <rihards.krislauks@Tilde.lv>
2015-06-30 13:31:07 +0000

    Display only one instance of RetryWarningForm at a time.
    git-tfs-id: [http://tfs:8080/tfs/DefaultCollection]$/LetsMT!/SDLTradosPlugin;C27262

--- LetsMTTranslationProviderLanguageDirection.cs
+++ LetsMTTranslationProviderLanguageDirection.cs
@@ -29,6 +29,8 @@ namespace LetsMT.MTProvider
         #region "PrivateMembers"
         private LetsMTTranslationProvider _provider;
         private LanguagePair _languageDirection;
+        private static bool systemIsStarting = false;
+        private static AutoResetEvent startingResetEvent = new AutoResetEvent(true);
         #endregion
 
         #region "ITranslationProviderLanguageDirection Members"
@@ -321,6 +323,10 @@ namespace LetsMT.MTProvider
                             {
                                 var result = SearchTranslationUnit(settings, tu);
 
+                                systemIsStarting = false;
+                                // there was no "system is starting" exception. the system is up, allow other threads to continue
+                                startingResetEvent.Set();
+
                                 results.Add(result);
 
                                 tryAgain = false;
@@ -329,28 +335,49 @@ namespace LetsMT.MTProvider
                             {
                                 if (ex.Message.Contains("system is starting"))
                                 {
-                                     Form UForm = null;
-                                     if ((UForm = IsFormAlreadyOpen(typeof(RetryWarningForm))) != null)
-                                     {
-                                         UForm.Close();
-                                     }
-                                    RetryWarningForm warnForm = new RetryWarningForm();
-                                    DialogResult Result = warnForm.ShowDialog();
-                                    //DialogResult Result = MessageBox.Show("Automated system is starting up. Retry?", "System starting", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
-                                     if (Result == DialogResult.No)
-                                     {
-                                         _provider.m_userRetryWarning = false;
-                                         tryAgain = false;
-                                         results.Add(null);
-                                     }
-                                     else if (Result == DialogResult.Cancel)
-                                     {
-                                         //If user presses "X" or another process calls "close" function
-                                         //end process and return "null"
-                                         tryAgain = false;
-                                         results.Add(null);
-                                     }
-                                    
+                                    systemIsStarting = true;
+
+                                    // allow only one thread to proceed displaying the RetryWarningForm. all other threads must wait
+                                    startingResetEvent.WaitOne();
+
+                                    /* for completeness sake:
+                                     * a race condition may arise when one thread sets the systemIsStarting flag to false indicating that the next thread that passes here doesn't have to
+                                     * display the RetryWarningForm but another thread sets it to false again just before the following 'if (systemIsStarting)...' check.
+                                     * a fix might be to not trust the flag and ckeck again whether the system is starting up or not exclusively in this thread before displaying the
+                                     * RetryWarningForm, but this would amount to other changes in the code so we don't do that here. nothing bad happens anyway.
+                                     */
+
+                                    // check if the translation system hasn't been brought up by some other thread while this thread was waiting for the startingResetEvent to reset.
+                                    // only the first thread that passes here has systemIsStarting set to true. the threads that follow don't have to display the RetryWarningForm again
+                                    if (systemIsStarting)
+                                    {
+                                        Form UForm = null;
+                                        if ((UForm = IsFormAlreadyOpen(typeof(RetryWarningForm))) != null)
+                                        {
+                                            UForm.Close();
+                                        }
+                                        RetryWarningForm warnForm = new RetryWarningForm();
+                                        DialogResult Result = warnForm.ShowDialog();
+                                        //DialogResult Result = MessageBox.Show("Automated system is starting up. Retry?", "System starting", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                                         if (Result == DialogResult.No)
+                                         {
+                                             _provider.m_userRetryWarning = false;
+                                             tryAgain = false;
+                                             results.Add(null);
+                                         }
+                                         else if (Result == DialogResult.Cancel)
+                                         {
+                                             //If user presses "X" or another process calls "close" function
+                                             //end process and return "null"
+                                             tryAgain = false;
+                                             results.Add(null);
+                                         }
+                                    }
+                                    else
+                                    {
+                                        // there was no need to show the RetryWarningForm. allow the next thread to proceed
+                                        startingResetEvent.Set();
+                                    }
                                 }
                                 else
                                 {
